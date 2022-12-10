91252a31e45844c4110d7754a2afe306e076ed69
seveibar <seveibar@gmail.com>
2022-12-10 13:36:39 -0800

    add drag and zoom to pcb layout

--- src/lib/components/DebugLayout.tsx
+++ src/lib/components/DebugLayout.tsx
@@ -1,5 +1,6 @@
 import { applyTransform } from "lib/apply-transform"
 import { getStandardObj } from "lib/get-standard-obj"
+import { useMouseMatrixTransform } from "use-mouse-matrix-transform"
 import { Layout } from "lib/types"
 import { useEffect, useRef, useState } from "react"
 import {
@@ -11,8 +12,6 @@ import {
   flipY,
 } from "transformation-matrix"
 import LayoutObject from "./LayoutObject"
-
-type Point = { x: number; y: number }
 
 const defaultTransform = compose(
   identity(),
@@ -35,81 +34,11 @@ const getTypeInd = (type: string) => {
 
 export default ({ layout }: { layout: Layout }) => {
   const [transform, setTransform] = useState<Matrix>(defaultTransform)
-  const canvasElm = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    if (!canvasElm.current) return
-    const elm = canvasElm.current
-    let init_tf = transform
-
-    let m0: Point = { x: 0, y: 0 },
-      m1: Point = { x: 0, y: 0 },
-      md = false
-
-    const getMousePos = (e: MouseEvent) => {
-      const rect = elm.getBoundingClientRect()
-      return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top,
-      }
-    }
-
-    function handleMouseDown(e: MouseEvent) {
-      m0 = getMousePos(e)
-      md = true
-      e.preventDefault()
-    }
-    function handleMouseUp(e: MouseEvent) {
-      m1 = getMousePos(e)
-
-      const new_tf = compose(translate(m1.x - m0.x, m1.y - m0.y), init_tf)
-      setTransform(new_tf)
-      init_tf = new_tf
-
-      md = false
-    }
-    function handleMouseMove(e: MouseEvent) {
-      if (!md) return
-      m1 = getMousePos(e)
-
-      setTransform(compose(translate(m1.x - m0.x, m1.y - m0.y), init_tf))
-    }
-    function handleMouseWheel(e: WheelEvent) {
-      const center = getMousePos(e)
-      const new_tf = compose(
-        translate(center.x, center.y),
-        scale(1 - e.deltaY / 1000, 1 - e.deltaY / 1000),
-        translate(-center.x, -center.y),
-        init_tf
-      )
-      setTransform(new_tf)
-      init_tf = new_tf
-      e.preventDefault()
-    }
-    function handleMouseOut(e: MouseEvent) {
-      if (!md) return
-      md = false
-      m1 = getMousePos(e)
-
-      const new_tf = compose(translate(m1.x - m0.x, m1.y - m0.y), init_tf)
-      setTransform(new_tf)
-      init_tf = new_tf
-    }
-
-    elm.addEventListener("mousedown", handleMouseDown)
-    elm.addEventListener("mouseup", handleMouseUp)
-    elm.addEventListener("mousemove", handleMouseMove)
-    elm.addEventListener("mouseout", handleMouseOut)
-    elm.addEventListener("wheel", handleMouseWheel)
-
-    return () => {
-      elm.removeEventListener("mousedown", handleMouseDown)
-      elm.removeEventListener("mouseup", handleMouseUp)
-      elm.removeEventListener("mousemove", handleMouseMove)
-      elm.removeEventListener("mouseout", handleMouseOut)
-      elm.removeEventListener("wheel", handleMouseWheel)
-    }
-  }, [canvasElm.current])
+  const { ref } = useMouseMatrixTransform({
+    transform,
+    onSetTransform: setTransform,
+  })
 
   const layout_objects = [...layout.objects]
 
@@ -123,7 +52,7 @@ export default ({ layout }: { layout: Layout }) => {
   return (
     <div>
       <div
-        ref={canvasElm}
+        ref={ref}
         style={{
           position: "relative",
           backgroundColor: "#eee",
--- src/lib/components/PCBLayout.tsx
+++ src/lib/components/PCBLayout.tsx
@@ -2,32 +2,38 @@ import { Layout } from "lib/types"
 import { useMeasure } from "react-use"
 import * as PCBViewer from "@tscircuit/pcb-viewer"
 import { identity, compose, scale, translate } from "transformation-matrix"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
+import useMouseMatrixTransform from "use-mouse-matrix-transform"
+
+const defaultTransform = compose(translate(400, 300), scale(40, 40))
 
 export default ({ layout }: { layout: Layout }) => {
   const [ref, refDimensions] = useMeasure()
-  const transform = useMemo(() => {
-    const transform = compose(translate(400, 300), scale(40, 40))
-    return transform
-  }, [])
+  const [transform, setTransform] = useState(defaultTransform)
+  const { ref: transformRef } = useMouseMatrixTransform({
+    transform,
+    onSetTransform: setTransform,
+  })
   return (
-    <div ref={ref as any}>
-      <PCBViewer.CanvasElementsRenderer
-        key={refDimensions.width}
-        elements={layout.objects as any}
-        transform={transform}
-        height={600}
-        width={refDimensions.width}
-        grid={{
-          spacing: 1,
-          view_window: {
-            left: 0,
-            right: refDimensions.width || 500,
-            top: 600,
-            bottom: 0,
-          },
-        }}
-      />
+    <div ref={transformRef as any}>
+      <div ref={ref as any}>
+        <PCBViewer.CanvasElementsRenderer
+          key={refDimensions.width}
+          elements={layout.objects as any}
+          transform={transform}
+          height={600}
+          width={refDimensions.width}
+          grid={{
+            spacing: 1,
+            view_window: {
+              left: 0,
+              right: refDimensions.width || 500,
+              top: 600,
+              bottom: 0,
+            },
+          }}
+        />
+      </div>
     </div>
   )
 }
